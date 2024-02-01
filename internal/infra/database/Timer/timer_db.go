cdc3696606be79453d51eba062a740628f34efdf
Marcos Duarte <pe.marcos30@gmail.com>
2024-02-01 14:19:23 -0300

    Refactor timer_db

--- internal/infra/database/Timer/timer_db.go
+++ internal/infra/database/Timer/timer_db.go
@@ -11,6 +11,8 @@ type Timer struct {
 	DB *gorm.DB
 }
 
+const PreLoadTaskProject = "Task.Project"
+
 func NewTimer(db *gorm.DB) *Timer {
 	return &Timer{DB: db}
 }
@@ -28,7 +30,7 @@ func (p *Timer) FindMyTimers(page, limit int, sort string) ([]*entity.Timer, err
 	if page != 0 && limit != 0 {
 		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("task_id " + sort).Find(&timer).Error
 	} else {
-		err = p.DB.Order("task_id " + sort).Preload("Task").Preload("Task.Project").Find(&timer).Error
+		err = p.DB.Order("task_id " + sort).Preload("Task").Preload(PreLoadTaskProject).Find(&timer).Error
 	}
 	return timer, err
 }
@@ -36,7 +38,7 @@ func (p *Timer) FindMyTimers(page, limit int, sort string) ([]*entity.Timer, err
 func (p *Timer) FindByTaskID(taskID string) ([]*entity.Timer, error) {
 	var timer []*entity.Timer
 
-	err := p.DB.Preload("Task").Preload("Task.Project").Where("task_id = ?", taskID).Find(&timer).Error
+	err := p.DB.Preload("Task").Preload(PreLoadTaskProject).Where("task_id = ?", taskID).Find(&timer).Error
 
 	return timer, err
 }
@@ -50,7 +52,7 @@ func (p *Timer) FindByID(id string) (*entity.Timer, error) {
 func (p *Timer) FindByDate(date time.Time) ([]*entity.Timer, error) {
 	var timer []*entity.Timer
 
-	err := p.DB.Where("DATE(start_time) = ? OR DATE(end_time) = ?", date.Format("2006-01-02"), date.Format("2006-01-02")).Preload("Task").Preload("Task.Project").Find(&timer).Error
+	err := p.DB.Where("DATE(start_time) = ? OR DATE(end_time) = ?", date.Format("2006-01-02"), date.Format("2006-01-02")).Preload("Task").Preload(PreLoadTaskProject).Find(&timer).Error
 
 	return timer, err
 }
