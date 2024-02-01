b705cd69cadb7485483762965b6c5d734c4264dc
Marcos Duarte <pe.marcos30@gmail.com>
2024-02-01 14:14:09 -0300

    Define constant on project_test

--- internal/entity/project_test.go
+++ internal/entity/project_test.go
@@ -7,19 +7,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const ProjectName = "Project 1"
+const ProjectDescription = "Description"
+const ProjectColor = "#eb4034"
+const ProjectID = "1"
+
 func TestNewProject(t *testing.T) {
-	p, err := NewProject("Project 1", "#eb4034", "Description", "1")
+	p, err := NewProject(ProjectName, ProjectColor, ProjectDescription, ProjectID)
 
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
-	assert.Equal(t, "Project 1", p.Name)
-	assert.Equal(t, "#eb4034", p.Color)
-	assert.Equal(t, "Description", p.Description)
+	assert.Equal(t, ProjectName, p.Name)
+	assert.Equal(t, ProjectColor, p.Color)
+	assert.Equal(t, ProjectDescription, p.Description)
 }
 
 func TestProjectWhenNameIsRequired(t *testing.T) {
-	p, err := NewProject("", "#eb4034", "Description", "1")
+	p, err := NewProject("", ProjectColor, ProjectDescription, ProjectID)
 
 	assert.Nil(t, p)
 	assert.NotNil(t, err)
@@ -27,21 +32,21 @@ func TestProjectWhenNameIsRequired(t *testing.T) {
 }
 
 func TestProjectWhenColorIsRequired(t *testing.T) {
-	p, err := NewProject("Project 1", "", "Description", "1")
+	p, err := NewProject(ProjectName, "", ProjectDescription, ProjectID)
 	assert.Nil(t, p)
 	assert.NotNil(t, err)
 	assert.Equal(t, errors.ErrColorIsRequired, err)
 }
 
 func TestProjectWhenDescriptionIsRequired(t *testing.T) {
-	p, err := NewProject("Project 1", "#eb4034", "", "1")
+	p, err := NewProject(ProjectName, ProjectColor, "", ProjectID)
 	assert.Nil(t, p)
 	assert.NotNil(t, err)
 	assert.Equal(t, errors.ErrDescriptionIsRequired, err)
 }
 
 func TestProjectValidate(t *testing.T) {
-	p, err := NewProject("Project 1", "#eb4034", "Description", "1")
+	p, err := NewProject(ProjectName, ProjectColor, ProjectDescription, ProjectID)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.Nil(t, p.Validate())
